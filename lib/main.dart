d6d6aeda21027b7cc3ae048e84f0ad38e6989e95
Lebeil <liedel75@gmail.com>
2021-10-08 20:04:19 +0200

    fix ruby version & add textSection

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'signup_page.dart';
 
 void main() {
@@ -16,24 +17,28 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Colors.blue.shade200,
-                Colors.blueAccent.shade700,
-                Colors.blue.shade900,
-              ],
-            ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.blue.shade200,
+              Colors.blueAccent.shade700,
+              Colors.blue.shade900,
+            ],
           ),
-          child: Center(
-              child: Column(
+        ),
+        child: Center(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               flutterIcon,
+              titleSection,
+              textSection,
             ],
-          ))),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -63,5 +68,45 @@ Widget flutterIcon = Container(
     ),
     child: Image.network(
         'https://drissas.com/wp-content/uploads/2021/08/flutter-logo.png'),
+  ),
+);
+
+Widget titleSection = Container(
+  padding: const EdgeInsets.all(30),
+  margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text(
+        'Flutter',
+        style: GoogleFonts.exo(
+          fontSize: 40,
+          color: Colors.white,
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+      const SizedBox(
+        width: 3,
+      ),
+      Text(
+        "login",
+        style: GoogleFonts.exo(
+          fontSize: 40,
+          color: Colors.lightGreenAccent[400],
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+    ],
+  ),
+);
+
+Widget textSection = Container(
+  padding: const EdgeInsets.fromLTRB(0, 5, 0, 50),
+  child: Text(
+    "Page de connexion",
+    style: GoogleFonts.comfortaa(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+    ),
   ),
 );
