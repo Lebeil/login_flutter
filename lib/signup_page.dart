130edd37b0c933acf374685afeca483201021dec
Lebeil <liedel75@gmail.com>
2021-10-08 22:19:20 +0200

    add page inscription

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/main.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignupPage extends StatelessWidget {
   const SignupPage({Key? key}) : super(key: key);
@@ -6,15 +8,277 @@ class SignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Deuxième page"),
+      appBar: AppBar(
+        title: const Text("Inscription"),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.blue.shade200,
+              Colors.blueAccent.shade700,
+              Colors.blue.shade900,
+            ],
+          ),
         ),
-        body: Center(
-            child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('retour'),
-        )));
+        child: Column(
+          children: [
+            topSection,
+            inputSection,
+            signupButton,
+          ],
+        ),
+      ),
+    );
   }
 }
+
+Widget topSection = Container(
+  margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text(
+        'Déjà un compte ?',
+        style: GoogleFonts.comfortaa(color: Colors.white),
+      ),
+      const BackButton(),
+    ],
+  ),
+);
+
+class BackButton extends StatelessWidget {
+  const BackButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      child: Text(
+        "Connectez-vous",
+        style: GoogleFonts.comfortaa(color: Colors.white),
+      ),
+    );
+  }
+}
+
+Widget inputSection = Container(
+  margin: const EdgeInsets.all(30),
+  child: Column(
+    children: [
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(30),
+          border: Border.all(width: 1, color: Colors.white30),
+        ),
+        height: 60,
+        child: Row(
+          children: [
+            Container(
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Icon(
+                Icons.people_outline,
+                size: 30,
+                color: Colors.blue[900],
+              ),
+            ),
+            SizedBox(
+              height: 60,
+              width: 230,
+              child: Center(
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  obscureText: false,
+                  style: GoogleFonts.comfortaa(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Pseudo',
+                    hintStyle: GoogleFonts.comfortaa(
+                      color: Colors.white,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      const SizedBox(height: 30),
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(30),
+          border: Border.all(width: 1, color: Colors.white30),
+        ),
+        height: 60,
+        child: Row(
+          children: [
+            Container(
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Icon(
+                Icons.mail_outline,
+                size: 30,
+                color: Colors.blue[900],
+              ),
+            ),
+            SizedBox(
+              height: 60,
+              width: 230,
+              child: Center(
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  obscureText: false,
+                  style: GoogleFonts.comfortaa(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    hintStyle: GoogleFonts.comfortaa(
+                      color: Colors.white,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      const SizedBox(height: 30),
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(30),
+          border: Border.all(width: 1, color: Colors.white30),
+        ),
+        height: 60,
+        child: Row(
+          children: [
+            Container(
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Icon(
+                Icons.lock_outline,
+                size: 30,
+                color: Colors.blue[900],
+              ),
+            ),
+            SizedBox(
+              height: 60,
+              width: 230,
+              child: Center(
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  obscureText: true,
+                  style: GoogleFonts.comfortaa(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Mot de passe',
+                    hintStyle: GoogleFonts.comfortaa(
+                      color: Colors.white,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      const SizedBox(height: 30),
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(30),
+          border: Border.all(width: 1, color: Colors.white30),
+        ),
+        height: 60,
+        child: Row(
+          children: [
+            Container(
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Icon(
+                Icons.date_range,
+                size: 30,
+                color: Colors.blue[900],
+              ),
+            ),
+            SizedBox(
+              height: 60,
+              width: 230,
+              child: Center(
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  obscureText: false,
+                  style: GoogleFonts.comfortaa(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Date de naissance',
+                    hintStyle: GoogleFonts.comfortaa(
+                      color: Colors.white,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  ),
+);
+
+Widget signupButton = ElevatedButton(
+  style: ElevatedButton.styleFrom(
+    primary: Colors.white,
+    padding: const EdgeInsets.fromLTRB(110, 15, 110, 15),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(30)),
+    ),
+  ),
+  child: Text(
+    "Inscription".toUpperCase(),
+    style: TextStyle(
+      color: Colors.blue[900],
+      fontSize: 20.0,
+    ),
+  ),
+  onPressed: () {},
+);
